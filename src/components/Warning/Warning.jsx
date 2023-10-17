fccc049bbebe073e0bf8646b06a56bbc931b54df
Supriyaratnaparkhe <91657954+Supriyaratnaparkhe@users.noreply.github.com>
2023-10-17 09:27:11 +0530

    Update Warning.jsx

--- src/components/Warning/Warning.jsx
+++ src/components/Warning/Warning.jsx
@@ -4,7 +4,7 @@ import Vector from "../assets/Vector.png";
 const Warning = () => {
   return (
     <div className="warning-message">
-      <img src={Vector} alt="React Image" /> &nbsp; Minimum 3 category required
+      <img src={Vector} alt="noimg" /> &nbsp; Minimum 3 category required
     </div>
   );
 };
